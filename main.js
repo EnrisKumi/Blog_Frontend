ae63a4615ea5fbe24594bc6ff6086fb6943b3c26
EnrisKumi <ekumi20@epoka.edu.al>
2023-01-12 21:20:15 +0100

    Manage categories & manage users pages

--- main.js
+++ main.js
@@ -16,3 +16,23 @@ const closeNav = () => {
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav)
+
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show_sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide_sidebar-btn');
+
+const showSidebar = () =>{
+    sidebar.style.left = '0'
+    showSidebarBtn.style.display = 'none'
+    hideSidebarBtn.style.display = 'inline-block'
+}
+
+const hideSidebar = () =>{
+    sidebar.style.left = '-100%'
+    showSidebarBtn.style.display = 'inline-block'
+    hideSidebarBtn.style.display = 'none'
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
